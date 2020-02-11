a2f14d4edf0d599157944531f0a28f8dd112cd20
Nicole white <you@example.com>
2020-02-11 10:43:39 -1000

    get req to posts working, post to comments returning 500

--- ExpressRouter.js
+++ ExpressRouter.js
@@ -19,7 +19,37 @@ router.post('/', (req, res) =>{
             res.status(500).json({errorMessage: "There was an error while saving the post to the database"})
         })
     }
+});
+
+router.get('/', (req, res) =>{
+    Data.find()
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(err =>{
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
 })
+
+// router.post('/:id/comments', (req, res) =>{
+//     const { id } = req.params;
+//     const { text } = req.body;
+//     if(!text){
+//         res.status(400).json({errorMessage: "Please provide text for the comment."})
+//     } else{
+//         Data.insertComment(id, req.body)
+//         .then(data =>{
+//             if(!data){
+//                 res.status(404).json({message: "The post with the specified ID does not exist."})
+//             } else{
+//                 res.status(200).json(data)
+//             }
+//         })
+//         .catch(err =>{
+//             res.status(500).json({})
+//         })
+//     }
+// }); //sending 500
 
 
 module.exports = router;
